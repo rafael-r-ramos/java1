f34a5bd599e32112fc909d57e40edc861048a31d
User <rafaelribeiroramos96@gmail.com>
2022-07-20 15:05:44 -0300

    Criação do Projeto

---
+++ primeiro-programa-java/src/cursojava/executabel/PrimeiraClasseJava.java
@@ -0,0 +1,10 @@
+package cursojava.executabel;
+
+public class PrimeiraClasseJava {
+	/*Main é um metodo auto executavel em java*/
+	public static void main(String[] args) {
+		
+		System.out.println("Ola mundo!");
+	}
+
+}
